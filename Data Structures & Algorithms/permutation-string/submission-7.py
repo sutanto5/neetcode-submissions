8e9c31499672ccdc786684da596c7c30368cc9b6
Joshua Sutanto <SUTANTO5@ILLINOIS.EDU>
2026-07-19 18:45:15 -0500

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.py
@@ -0,0 +1,34 @@
+from collections import Counter
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        if len(s1) > len(s2):
+            return False
+
+        # compare windows
+        window_len = len(s1)
+
+        current_window = Counter(s2[:window_len])
+        target = Counter(s1)
+
+        if current_window == target:
+            return True
+
+
+        for i in range(window_len, len(s2)):
+
+            # add right
+            current_window[s2[i]] += 1
+
+            # remove left
+            removed_char = s2[i - window_len]
+            current_window[removed_char] -= 1
+
+            if current_window[removed_char] == 0:
+                del current_window[removed_char]
+
+            if current_window == target:
+                return True
+        
+        return False
