d2e264ee76d65099c75d372239981d5de1af9656
Joshua Sutanto <SUTANTO5@ILLINOIS.EDU>
2026-08-12 13:15:05 -0500

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+
+        s = re.sub(r'[^a-zA-Z0-9]', '', s)
+        s = s.lower()
+
+        start = 0
+        end = len(s) - 1
+
+        while start < end:
+            if s[start] != s[end]:
+                return False
+            else:
+                start += 1
+                end -=1
+        return True
